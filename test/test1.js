09b64588b8bddbcc600c89fd52d43a1ce79b3134
NicoRomero3210 <nicoromero3210@gmail.com>
2019-05-26 00:14:23 -0300

    prueba final japish japish

--- test/test1.js
+++ test/test1.js
@@ -6,18 +6,25 @@ var request = request("http://localhost:4000");
 describe("Aplicacion Express", () => {
 
 
-	afterEach(function(){
-
-		server.close();
-
-	});
+	
 
 	it("Tirame una sooga Japish japish" , (done) => {
 
 		//request.post('/links/add').send({valor1:"5",valor2:"2"}).expect(200,done);
 		request.post('/links/add').send({valor1:"2",valor2:"2"}).expect(200,done);
+		
 
-		})
+
+		});
+
+	after(function(done){
+
+		server.close();
+		done();
+
+	});
+
+	//server.close();
 
 	})
 
