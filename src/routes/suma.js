c0665b796f1dca40cf0851584a1b9a27dcfaf5b5
NicoRomero3210 <nicoromero3210@gmail.com>
2019-05-26 23:59:30 -0300

    sigo probando lpm

--- src/routes/suma.js
+++ src/routes/suma.js
@@ -1,6 +1,22 @@
 sumar = function(valor1,valor2){
 	
-	return valor1+valor2;
+	if((!isNaN(valor2))&&(!isNaN(valor1))&&(valor1!="")&&(valor2!="")){
+		
+		result = Number(valor1) + Number(valor2);	
+		//res.status(200).json({dato: (typeof result)});
+		//res.send(200);	
+
+		
+		
+		
+	}
+	else{
+
+		result = "" + "Alguno de tus datos no es numerico amigueroouuu";
+		//res.status(200).json({dato: (typeof result)});
+	
+	}
+	return result;
 
 }
 
--- test/test1.js
+++ test/test1.js
@@ -2,16 +2,30 @@ var assert = require('assert');
 var request = require('supertest');
 var server = require('../src/index.js');
 var request = request("http://localhost:4000");
+var suma = require('../src/routes/suma.js');
+var chai = require('chai');
+var assert2 = chai.assert;
 
 describe("Aplicacion Express", () => {
 
 
 	
 
-	it("Tirame una sooga Japish japish" , (done) => {
+	it("Tirame una sooga" , () => {
 
 		//request.post('/links/add').send({valor1:"5",valor2:"2"}).expect(200,done);
-		request.post('/links/add').send({valor1:"a",valor2:"2"}).expect(200,done);
+		//request.post('/links/add').send({valor1:"3",valor2:"5"}).expect(200,{"dato": "number"},done);
+		result = suma("r","5");
+		
+
+
+		//request.post('/links/add').send({valor1:"r",valor2:"5"}).expect(function(res){assert.equal(res.locals.dato,"string")}).end(done);
+		
+
+
+		assert2.equal((!isNaN(result)),true);
+
+		//done();
 		
 
 
