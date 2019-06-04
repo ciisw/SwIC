24e97c97c3d1959ce6ca01dbfb724bcd24ed7be6
NicoRomero3210 <nicoromero3210@gmail.com>
2019-06-04 00:02:02 -0300

    cambio titulo2

--- src/routes/suma.js
+++ src/routes/suma.js
@@ -2,14 +2,14 @@ sumar = function(valor1,valor2){
 	
 	var result;
 	
-	/*if((!isNaN(valor2))&&(!isNaN(valor1))&&(valor1!="")&&(valor2!="")){
+	if((!isNaN(valor2))&&(!isNaN(valor1))&&(valor1!="")&&(valor2!="")){
 		
 		result = Number(valor1) + Number(valor2);
 		//result = valor1 +valor2;
 
-	}*/
+	}
 
-	result = Number(valor1) + Number(valor2);
+	//result = Number(valor1) + Number(valor2);
 	//result = valor1 +valor2;
 
 	//console.log(typeof result);
