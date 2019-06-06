c8ee0d43c1f146df90538e09fb1ade26a43f7c1d
Edgar <edgar07cardozo@hotmail.com>
2019-06-06 14:58:00 -0300

    Ahora anda

--- src/routes/suma.js
+++ src/routes/suma.js
@@ -2,15 +2,15 @@ sumar = function(valor1,valor2){
 	
 	var result;
 	
-	/*if((!isNaN(valor2))&&(!isNaN(valor1))&&(valor1!="")&&(valor2!="")){
+	if((!isNaN(valor2))&&(!isNaN(valor1))&&(valor1!="")&&(valor2!="")){
 		
 		result = Number(valor1) + Number(valor2);
 		//result = valor1 +valor2;
 
-	}*/
+	}
 
 	//result = Number(valor1) + Number(valor2);
-	result = valor1 +valor2;
+	//result = valor1 +valor2;
 
 	//console.log(typeof result);
 	return result;
